45c70eb40471584a1f54e8560d49e8aca4ae99fb
Andrew Starr <astarr93@outlook.com>
2020-10-09 03:33:37 -0400

    Update server.js
    Added starting routes to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,20 +1,38 @@
-// const app = express();
-// const express = require("express");
-// const fs = require("fs");
-// const path = require('path');
-// const port = process.env.PORT || 3000;
+const app = express();
+const express = require("express");
+const fs = require("fs");
+const path = require('path');
+const port = process.env.PORT || 3000;
 
 
-const http = require('http');
 
-const PORT = 8080;
 
-function handleRequest(request, response) {
-    response.end('It works! Path hit:'  + request.url);
-};
 
-const server = http.createServer(handleRequest);
+app.get("/notes", function (req, res) {
+    res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
 
-server.listen(PORT, () => {
-    console.log('server listening on: http://localhost:' + PORT);
-})
+app.get("/api/notes", function (req, res) {
+    notes = fs.readFileSync("./db/db.json");
+    res.json(JSON.parse(notes));
+});
+
+app.post("/api/notes", function (req, res) {
+    notes = fs.readFileSync("./db/db.json");
+    res.json(JSON.parse(notes));
+});
+
+app.post("/api/notes", function (req, res) {
+    notes = fs.readFileSync(".db/db.json");
+    notes = JSON.parse(notes);
+    let newNotes = req.body;
+    newNotes["id"] = newNotes.title;
+    notes.push(newNotes);
+    fs.writeFile("./db/db.json", notes, function (err) {
+        if (err) {
+            return console.log(err);
+        }
+        console.log("Successfully updated!");
+    });
+    res.json(notes);
+});
