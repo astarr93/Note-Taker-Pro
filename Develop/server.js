ff3296cfffa4ad077c52880e633a4cf4f85411c0
Andrew Starr <astarr93@outlook.com>
2020-10-09 22:27:01 -0400

    Update server.js
    Starting fresh on server.js after getting a little more familarized with the documentation.

--- Develop/server.js
+++ Develop/server.js
@@ -4,35 +4,5 @@ const fs = require("fs");
 const path = require('path');
 const port = process.env.PORT || 3000;
 
-
-
-
-
-app.get("/notes", function (req, res) {
-    res.sendFile(path.join(__dirname, "/public/notes.html"));
-});
-
-app.get("/api/notes", function (req, res) {
-    notes = fs.readFileSync("./db/db.json");
-    res.json(JSON.parse(notes));
-});
-
-app.post("/api/notes", function (req, res) {
-    notes = fs.readFileSync("./db/db.json");
-    res.json(JSON.parse(notes));
-});
-
-app.post("/api/notes", function (req, res) {
-    notes = fs.readFileSync(".db/db.json");
-    notes = JSON.parse(notes);
-    let newNotes = req.body;
-    newNotes["id"] = newNotes.title;
-    notes.push(newNotes);
-    fs.writeFile("./db/db.json", notes, function (err) {
-        if (err) {
-            return console.log(err);
-        }
-        console.log("Successfully updated!");
-    });
-    res.json(notes);
-});
+app.use(express.static('public'));
+app.use(express.json);
