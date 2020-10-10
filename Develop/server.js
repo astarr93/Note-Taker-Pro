48d92823794dc131d2f185c2c190c562331dfcc9
Andrew Starr <astarr93@outlook.com>
2020-10-09 23:28:49 -0400

    Update server.js
    Added express, path, and fs modules. Created var for express instance. Set app to listen to port 3000

--- Develop/server.js
+++ Develop/server.js
@@ -1,12 +1,17 @@
-const http = require('http');
-const PORT = process.env.PORT || 3000;
-const server = http.createServer(handleRequest);
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
 
-server.listen(PORT, () => {
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(express.json());
+app.use(express.static('public'));
+
+app.listen(PORT, () => {
     console.log("Server listening on: http://localhost:" + PORT);
 })
 
-function handleRequest(req, res) {
-    res.end("It worked!" + req.url);
-}
 
