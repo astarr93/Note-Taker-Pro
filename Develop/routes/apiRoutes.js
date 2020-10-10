8d9d9d81f58ba20e4b41fdd589a7759329ea7ea9
Andrew Starr <astarr93@outlook.com>
2020-10-10 04:06:51 -0400

    Add api routes
    Added API for getting notes and posting

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,11 +1,29 @@
 const path = require("path");
+const fs = require('fs');
+const db = require("../db/db.json");
+
+let notes = fs.readFileSync("./db/db.json");
 
 module.exports = function (app) {
-    app.get("/notes", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/notes.html"));
+    app.get("/api/notes", function (req, res) {
+        res.json(JSON.parse(notes));
     });
 
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"))
+    app.post("/api/notes", function (req, res) {
+        let newNote = req.body;
+        notes.push(newNote);
+        fs.writeFileSync(db, new, function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            else {
+                res.status(200);
+            };
+        });
+        res.json(notes);
     });
+
+    // app.delete("/api/notes/:id", function (req, res) {
+    //     res.sendFile(path.join(__dirname, "../public/index.html"))
+    // });
 };
--- Develop/server.js
+++ Develop/server.js
@@ -1,10 +1,7 @@
-const db = require("./db/db.json");
-const fs = require('fs');
-const path = require('path');
 const express = require('express');
+const PORT = process.env.PORT || 3000;
 
 const app = express();
-const PORT = process.env.PORT || 3000;
 
 // Setup express.js middleware for JSON content-type and serve index.html
 app.use(express.urlencoded({ extended: true }));
@@ -12,7 +9,7 @@ app.use(express.json());
 app.use(express.static('public'));
 
 require("./routes/htmlRoutes")(app);
-// require("./routes/apiRoutes")(app);
+require("./routes/apiRoutes")(app);
 
 app.listen(PORT, () => {
     console.log("Server listening on: http://localhost:" + PORT);
