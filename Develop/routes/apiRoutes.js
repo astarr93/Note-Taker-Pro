9d5d3a285e921f158904df7394e6a4eecc13f936
Andrew Starr <astarr93@outlook.com>
2020-10-10 04:42:57 -0400

    Reset DB.json
    Reset db.json; Still working through API routes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,29 +1,30 @@
 const path = require("path");
 const fs = require('fs');
-const db = require("../db/db.json");
-
-let notes = fs.readFileSync("./db/db.json");
 
 module.exports = function (app) {
+
+    let notes = fs.readFileSync("./db/db.json", "utf8");
+
     app.get("/api/notes", function (req, res) {
         res.json(JSON.parse(notes));
     });
 
     app.post("/api/notes", function (req, res) {
         let newNote = req.body;
-        notes.push(newNote);
-        fs.writeFileSync(db, newNote, function (err) {
-            if (err) {
-                return console.log(err);
-            }
-            else {
-                res.status(200);
-            };
-        });
-        res.json(notes);
+        console.log(notes);
+        // notes.push(newNote);
+        // fs.writeFileSync("./db/db.json", newNote, function (err) {
+        //     if (err) {
+        //         return console.log(err);
+        //     }
+        //     else {
+        //         res.status(200);
+        //     };
+        // });
+        // res.json(notes);
     });
 
-    // app.delete("/api/notes/:id", function (req, res) {
-    //     res.sendFile(path.join(__dirname, "../public/index.html"))
-    // });
+    app.delete("/api/notes/:id", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"))
+    });
 };
