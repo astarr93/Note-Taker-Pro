5f533315676e9649484c04f03c4c1714a97ce11a
Andrew Starr <astarr93@outlook.com>
2020-10-09 22:46:23 -0400

    Update server.js
    Cleared code to create server file from scratch. Created starting server to listen on port 3000 and handle a request

--- Develop/server.js
+++ Develop/server.js
@@ -1,8 +1,12 @@
-const app = express();
-const express = require("express");
-const fs = require("fs");
-const path = require('path');
-const port = process.env.PORT || 3000;
+const http = require('http');
+const PORT = process.env.PORT || 3000;
+const server = http.createServer(handleRequest);
 
-app.use(express.static('public'));
-app.use(express.json);
+server.listen(PORT, () => {
+    console.log("Server listening on: http://localhost:" + PORT);
+})
+
+function handleRequest(req, res) {
+    res.end("It worked!" + req.url);
+}
+
