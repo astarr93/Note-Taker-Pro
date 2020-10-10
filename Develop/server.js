9738881b9aa1b9467b28e5ca1be8029ec88731ba
Andrew Starr <astarr93@outlook.com>
2020-10-10 03:13:02 -0400

    Added HTML routes
    Added HTML and API routes. Added database file to server.js

--- Develop/server.js
+++ Develop/server.js
@@ -1,17 +1,21 @@
-const express = require('express');
-const path = require('path');
+const db = require("./db/db.json");
 const fs = require('fs');
+const path = require('path');
+const express = require('express');
+const router = express.Router();
 
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
+// Setup express.js middleware for JSON content-type and serve index.html
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
+require("./routes/apiRoutes")(app);
+require("./routes/htmlRoutes")(app);
+
 app.listen(PORT, () => {
     console.log("Server listening on: http://localhost:" + PORT);
-})
-
-
+});
