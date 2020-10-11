fa05aa11da19993c8961de652e5dcce423c2bc3d
Andrew Starr <astarr93@outlook.com>
2020-10-10 20:44:56 -0400

    Update htmlRoutes.js
    Removed duplicate route.

--- Develop/routes/htmlRoutes.js
+++ Develop/routes/htmlRoutes.js
@@ -1,11 +1,7 @@
 const path = require("path");
 
 module.exports = function (app) {
-
-    app.get("/", function (req, res) {
-        res.sendFile(path.join(__dirname, "public/assets/index.html"))
-    })
-
+    
     app.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
