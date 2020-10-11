6263aca72582c5b0a4f88acd0650648e00fdf118
Andrew Starr <astarr93@outlook.com>
2020-10-10 23:37:31 -0400

    Fixed test data in db.json
    Fixed test data in db file.

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,6 +1,7 @@
 const path = require("path");
 const fs = require('fs');
 const db = require('../db/db.json');
+const { v4: uuidv4 } = require("uuid");
 
 module.exports = function (app) {
 
@@ -12,20 +13,21 @@ module.exports = function (app) {
         res.json(JSON.parse(data));
     });
 
-    // app.post("/api/notes", function (req, res) {
-    //     let newNote = req.body;
-    //     console.log("Posting note...");
-    //     // notes.push(newNote);
-    //     // fs.writeFileSync("./db/db.json", newNote, function (err) {
-    //     //     if (err) {
-    //     //         return console.log(err);
-    //     //     }
-    //     //     else {
-    //     //         res.status(200);
-    //     //     };
-    //     // });
-    //     // res.json(notes);
-    // });
+    app.post("/api/notes", function (req, res) {
+        let newNote = req.body;
+        newNote["id"] = uuidv4();
+        console.log("Posting note...");
+        console.log(newNote);
+        fs.writeFileSync(path.join(__dirname, "../db/db.json"), json.stringify(newNote), function (err) {
+            if (err) {
+                return console.log(err);
+            }
+            else {
+                res.status(200);
+            };
+        });
+        // res.json(notes);
+    });
 
     app.delete("/api/notes/:id", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/index.html"))
