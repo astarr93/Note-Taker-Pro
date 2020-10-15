58ef4c24c8aeadb7c923b64990104049694f2f44
Andrew Starr <astarr93@outlook.com>
2020-10-15 01:12:31 -0400

    Got all the routes working!
    Updated nodemon dependency; Updated db.json data, Added working routes for api and html.
    
    When you delete a note, it doesn't reload the page :(

--- Develop/routes/app.js
+++ Develop/routes/app.js
@@ -1,31 +1,38 @@
 const path = require("path");
 const fs = require('fs');
 const { v4: uuidv4 } = require("uuid");
+let savedNotes = JSON.parse(fs.readFileSync("./db/db.json"));
 
 module.exports = function (app) {
+
 
     app.get("/notes", function (req, res) {
         res.sendFile(path.join(__dirname, "../public/notes.html"));
     });
 
-    app.get("*", function (req, res) {
-        res.sendFile(path.join(__dirname, "../public/index.html"))
-    });
-
     app.get("/api/notes", function (req, res) {
-        let notes = readFileSync("./db/db.json", "utf-8")
-        return JSON.parse(notes);
+        res.json(savedNotes);
     });
 
     app.post("/api/notes", function (req, res) {
         let newNote = req.body;
         newNote["id"] = uuidv4();
         console.log("Posting note...");
-        console.log(newNote);
+        savedNotes.push(newNote);
+        fs.writeFileSync("./db/db.json", JSON.stringify(savedNotes));
         res.end();
     });
 
-    // app.delete("/api/notes/:id", function (req, res) {
-    //     res.sendFile(path.join(__dirname, "../public/index.html"))
-    // });
+    app.delete("/api/notes/:id", function (req, res) {
+        console.log(req.params.id);
+        let updatedList = savedNotes.filter(note => {
+            return (note.id !== req.params.id);
+        });
+        fs.writeFileSync("./db/db.json", JSON.stringify(updatedList));
+        res.end();
+    });
+
+    app.get("*", function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/index.html"))
+    });
 };
